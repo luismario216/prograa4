d63a8d6a01fe35eda03e0c031a2ef18f2a95a594
luismario216 <luismarioclaros2002@gmail.com>
2022-04-03 11:47:54 -1100

    avances tarea

---
+++ primer_proyecto_laravel/tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+    content: [
+      './storage/views/*.php',
+      './resources/views/*.blade.php',
+      './resources/**/*.js',
+      './resources/**/*.vue',
+      './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
+    ],
+    darkMode: 'class',
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  }
+  
